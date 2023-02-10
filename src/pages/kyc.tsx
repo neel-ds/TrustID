5567a4b8b83f6c47c156e4263b882c7f3e1cb3b5
Neel <neelp0980@gmail.com>
2023-02-10 21:28:23 +0530

    UI updated

--- src/pages/kyc.tsx
+++ src/pages/kyc.tsx
@@ -7,17 +7,14 @@ import FileUpload from "../components/form-elements/file-upload";
 import Header from "../components/form-components/Header";
 import { useToast } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
-import jwt_decode from 'jwt-decode';
+import jwt_decode from "jwt-decode";
 
 const Kyc: NextPage = () => {
-
-  const [token, setToken] = useState("")
-  const [schema, setSchema] = useState("")
-  const [schemaLink, setClaimLink] = useState("")
-  const [dob, setDob] = useState("")
-  const [aadhar, setAadhar] = useState("")
-
-  
+  const [token, setToken] = useState("");
+  const [schema, setSchema] = useState("");
+  const [schemaLink, setClaimLink] = useState("");
+  const [dob, setDob] = useState("");
+  const [aadhar, setAadhar] = useState("");
 
   const [image, setImage] = useState("");
   const toast = useToast();
@@ -52,16 +49,16 @@ const Kyc: NextPage = () => {
     console.log(aadharNumberAndDOB?.dob);
 
     setAadhar(aadharNumberAndDOB?.aadharNumber.replace(/ /g, "") as any);
-    setDob(aadharNumberAndDOB?.dob as any) ;
+    setDob(aadharNumberAndDOB?.dob as any);
     setstatus("True");
   };
 
   useEffect(() => {
-    fetch('https://api-staging.polygonid.com/v1/orgs/sign-in', {
-      method: 'POST',
+    fetch("https://api-staging.polygonid.com/v1/orgs/sign-in", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'Accept-Encoding': 'application/json',
+        "Content-Type": "application/json",
+        "Accept-Encoding": "application/json",
       },
       body: JSON.stringify({
         email: process.env.EMAIL,
@@ -70,50 +67,49 @@ const Kyc: NextPage = () => {
     })
       .then((response) => response.json())
       .then(({ token }) => {
-        console.log("TOken is",token);
-        setToken(token)
+        console.log("TOken is", token);
+        setToken(token);
         const {
           account: { organization: issuerId },
-        } = jwt_decode(token) as any
+        } = jwt_decode(token) as any;
 
-        const tempSchemaLink = `https://api-staging.polygonid.com/v1/issuers/${issuerId}/schemas/${
-          process.env.SCHEMA_ID
-        }` 
-        setClaimLink(`${tempSchemaLink}/offers`)
-        return { token, tempSchemaLink }
+        const tempSchemaLink = `https://api-staging.polygonid.com/v1/issuers/${issuerId}/schemas/${process.env.SCHEMA_ID}`;
+        setClaimLink(`${tempSchemaLink}/offers`);
+        return { token, tempSchemaLink };
       })
       .then(({ token, tempSchemaLink }) => {
         fetch(tempSchemaLink, {
-          method: 'GET',
+          method: "GET",
           headers: {
-            'Content-Type': 'application/json',
-            'Accept-Encoding': 'application/json',
+            "Content-Type": "application/json",
+            "Accept-Encoding": "application/json",
             Authorization: `Bearer ${token}`,
           },
         })
           .then((response) => response.json())
-          .then((data) => setSchema(data))
-      })
-  }, [])
+          .then((data) => setSchema(data));
+      });
+  }, []);
 
-  
   const handleResults = (results: any) => {
-    console.log("Results", results)
+    console.log("Results", results);
     fetch(schemaLink, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'Accept-Encoding': 'application/json',
+        "Content-Type": "application/json",
+        "Accept-Encoding": "application/json",
         Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify({
         attributes: Object.keys(results).map((k) => {
-          const removeDashes = results[k].indexOf('-') !== 0
-          const val = removeDashes ? results[k].replaceAll('-', '') : results[k]
+          const removeDashes = results[k].indexOf("-") !== 0;
+          const val = removeDashes
+            ? results[k].replaceAll("-", "")
+            : results[k];
           return {
             attributeKey: k,
             attributeValue: parseInt(val),
-          }
+          };
         }),
       }),
     })
@@ -122,10 +118,10 @@ const Kyc: NextPage = () => {
         console.log(`https://platform-test.polygonid.com/claim-link/${id}`);
         window.open(
           `https://platform-test.polygonid.com/claim-link/${id}`,
-          '_newtab'
-        )
-      })
-  }
+          "_newtab"
+        );
+      });
+  };
 
   return (
     <>
@@ -135,85 +131,100 @@ const Kyc: NextPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="px-4 md:px-0 my-8 mx-auto max-w-[1080px]">
-        <div className="pt-5 pb-5 mx-auto max-w-7xl">
+      <main className="px-4 md:px-0 mx-auto max-w-[1080px]">
+        <div className="max-w-7xl mt-5 pt-5 pb-5 mx-auto">
           <Header heading="Claim DID" />
-          <div className="flex flex-col w-full text-center">
-            <div className="flex justify-center w-full py-4 overflow-x-hidden overflow-y-auto md:inset-0 md:h-full">
-              <div className="relative w-full h-full md:h-auto">
-                <div className="relative bg-white rounded-lg shadow backdrop-blur-sm bg-opacity-20 dark:bg-gray-700 dark:bg-opacity-20">
+          <div className="flex flex-col md:flex-row w-full text-center">
+            <div className="w-full md:w-1/2 mb-10 md:mb-0 md:p-4 overflow-x-hidden overflow-y-auto md:inset-0 justify-center flex md:h-full">
+              <div className="relative w-full h-full md:h-auto ">
+                <div className="relative bg-white rounded-lg shadow backdrop-blur-lg bg-opacity-20 dark:bg-gray-700 dark:bg-opacity-20">
                   <div className="px-6 py-6 lg:px-8">
                     <form className="space-y-6">
-                      <div className="flex flex-col md:flex-row md:space-x-5">
-                        <div className="w-full space-y-6 md:w-1/2 mb-7 md-mb-0">
-                          <div className="flex space-x-5">
-                            <FileUpload
-                              id="productimage"
-                              name="productimage"
-                              label="Proof of personhood"
-                              onChange={(e: any) => {
-                                e.preventDefault();
-                                const image = URL.createObjectURL(
-                                  e.target.files[0]
-                                );
-                                console.log(e.target.files[0]);
-                                setImage(image);
-                                const data = new FormData();
-                                data.append(
-                                  "file",
-                                  e.target.files[0],
-                                  e.target.files[0].name
-                                );
+                      <FileUpload
+                        id="productimage"
+                        name="productimage"
+                        label="Proof of Personhood"
+                        onChange={(e: any) => {
+                          e.preventDefault();
+                          const image = URL.createObjectURL(e.target.files[0]);
+                          console.log(e.target.files[0]);
+                          setImage(image);
+                          const data = new FormData();
+                          data.append(
+                            "file",
+                            e.target.files[0],
+                            e.target.files[0].name
+                          );
 
-                                const options = {
-                                  method: "POST",
-                                  headers: {
-                                    "X-RapidAPI-Key":
-                                      "b6c828fff1msh2511ee51b094d03p1e49e9jsn476e7135f1ba",
-                                    "X-RapidAPI-Host":
-                                      "ocr-nanonets.p.rapidapi.com",
-                                  },
-                                  body: data,
-                                };
+                          const options = {
+                            method: "POST",
+                            headers: {
+                              "X-RapidAPI-Key":
+                                "b6c828fff1msh2511ee51b094d03p1e49e9jsn476e7135f1ba",
+                              "X-RapidAPI-Host": "ocr-nanonets.p.rapidapi.com",
+                            },
+                            body: data,
+                          };
 
-                                fetch(
-                                  "https://ocr-nanonets.p.rapidapi.com/",
-                                  options
-                                )
-                                  .then((response) => response.json())
-                                  .then((response) => {
-                                    const rawData =
-                                      response.results[0].page_data[0].raw_text;
-                                    execute(rawData);
-                                    console.log(
-                                      response.results[0].page_data[0].raw_text
-                                    );
-                                  })
+                          fetch("https://ocr-nanonets.p.rapidapi.com/", options)
+                            .then((response) => response.json())
+                            .then((response) => {
+                              const rawData =
+                                response.results[0].page_data[0].raw_text;
+                              execute(rawData);
+                              console.log(
+                                response.results[0].page_data[0].raw_text
+                              );
+                            })
 
-                                  .catch((err) => console.error(err));
-                              }}
-                            />
-                            <Image
-                              src={image !== "" ? image : "/previewIcon.png"}
-                              alt="preview"
-                              width={200}
-                              height={200}
-                            />
-                          </div>
-                        </div>
-                      </div>
+                            .catch((err) => console.error(err));
+                        }}
+                      />
+                      <Image
+                        src={image !== "" ? image : "/preview.png"}
+                        alt="preview"
+                        width={150}
+                        height={150}
+                      />
                       <div className="max-w-[200px] flex m-auto">
-                      <button onClick={(e) => {
-                        e.preventDefault();
-        handleResults({"IDNUMBER": aadhar, "DOB": dob.split('/').reverse().join('/').replace(/\//g, '-')});
-        console.log("Token", aadhar, dob.split('/').reverse().join('/').replace(/\//g, '-'), token);
-      }}>Click Me</button>
-     
+                        <Button
+                          label="Submit"
+                          onClick={(e) => {
+                            e.preventDefault();
+                            handleResults({
+                              IDNUMBER: aadhar,
+                              DOB: dob
+                                .split("/")
+                                .reverse()
+                                .join("/")
+                                .replace(/\//g, "-"),
+                            });
+                            console.log(
+                              "Token",
+                              aadhar,
+                              dob
+                                .split("/")
+                                .reverse()
+                                .join("/")
+                                .replace(/\//g, "-"),
+                              token
+                            );
+                          }}
+                        />
                       </div>
                     </form>
                   </div>
                 </div>
               </div>
+            </div>
+            <div>
+              <Image
+                src="/register.png"
+                className="rounded-full"
+                width="500"
+                height="400"
+                alt="Register"
+              />
             </div>
           </div>
         </div>
